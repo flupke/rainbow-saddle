97cd482b1239d78448b06e13919a58e04627201b
Luper Rouch <luper.rouch@gmail.com>
2015-11-28 20:22:47 +0100

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.2'
+version = '0.2.1'
 
 install_requires = [
     'psutil==0.6.1',
