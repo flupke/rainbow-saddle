a6a731b39425ffd1abe8e0e3198e2c1688b463a7
Luper Rouch <luper.rouch@gmail.com>
2015-12-29 09:37:22 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@
 from setuptools import setup, find_packages
 import os
 
+
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
-
-
-version = '0.2.1'
-
+version = '0.3.0'
 install_requires = [
     'psutil>=3.3.0,<=3.4',
 ]
@@ -16,10 +14,12 @@ install_requires = [
 
 setup(name='rainbow-saddle',
     version=version,
-    description="A wrapper around gunicorn to handle graceful restarts correctly",
+    description='A wrapper around gunicorn to handle graceful restarts '
+            'correctly',
     long_description=README + '\n\n' + NEWS,
     classifiers=[
-      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
     ],
     keywords='gunicorn wrapper graceful restart',
     author='Luper Rouch',
